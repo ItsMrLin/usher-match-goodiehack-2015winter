48fa19a31cb27d968664c5e614b58b43c09966de
Richard Li <lichard49@gmail.com>
2015-02-28 15:06:28 -0500

    Add JSON format

--- db.php
+++ db.php
@@ -1,3 +1,12 @@
+<!-- 
+{
+    "userType": "student"/"mentor",
+    "answers":
+    [{
+        "columnLabel": "value"
+    }] }
+-->
+
 <?php
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
